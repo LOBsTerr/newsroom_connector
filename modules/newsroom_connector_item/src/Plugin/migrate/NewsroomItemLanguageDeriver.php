6a46832db1778110286fec1d4d8ada0d924b068e
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2020-11-30 17:41:35 +0100

    Update base URLs. Add language for newsroom items. Check not empty translations for newsroom items.

--- modules/newsroom_connector_item/src/Plugin/migrate/NewsroomItemLanguageDeriver.php
+++ modules/newsroom_connector_item/src/Plugin/migrate/NewsroomItemLanguageDeriver.php
@@ -15,6 +15,7 @@ class NewsroomItemLanguageDeriver extends BaseNewsroomLanguageDeriver {
    */
   protected function getDerivativeValues(array $base_plugin_definition, LanguageInterface $language, $language_code) {
     $language_id = $language->getId();
+    $base_plugin_definition['source']['item_selector'] = '//channel/item[count(title[@lang="' . $language_code . '"]) > 0]';
 
     $base_plugin_definition['process']['langcode'] = [
       'plugin' => 'default_value',
@@ -25,107 +26,107 @@ class NewsroomItemLanguageDeriver extends BaseNewsroomLanguageDeriver {
       [
         'name' => 'title',
         'field' => 'title',
-        'xpath' => 'title'
+        'xpath' => 'title',
       ],
       [
         'name' => 'body',
         'field' => 'field_newsroom_body/value',
-        'xpath' => 'infsonewsroom:FullContent'
+        'xpath' => 'infsonewsroom:FullContent',
       ],
       [
         'name' => 'teaser',
         'field' => 'field_newsroom_teaser',
-        'xpath' => 'infsonewsroom:BasicTeaser'
+        'xpath' => 'infsonewsroom:BasicTeaser',
       ],
       [
         'name' => 'see_also_text',
         'field' => 'field_newsroom_see_also/title',
-        'xpath' => 'infsonewsroom:ContextOtherLinkText'
+        'xpath' => 'infsonewsroom:ContextOtherLinkText',
       ],
       [
         'name' => 'see_also_url',
         'field' => 'field_newsroom_see_also/uri',
-        'xpath' => 'infsonewsroom:ContextOtherLinkUrl'
+        'xpath' => 'infsonewsroom:ContextOtherLinkUrl',
       ],
       [
         'name' => 'main_link',
         'field' => 'field_newsroom_main_link',
-        'xpath' => 'infsonewsroom:BasicUrl'
+        'xpath' => 'infsonewsroom:BasicUrl',
       ],
       [
         'name' => 'project_acronym',
         'field' => 'field_newsroom_project_acronym',
-        'xpath' => 'infsonewsroom:ContextProjectAcronym'
+        'xpath' => 'infsonewsroom:ContextProjectAcronym',
       ],
       [
         'name' => 'project_name',
         'field' => 'field_newsroom_project_name',
-        'xpath' => 'infsonewsroom:ContextProjectName'
+        'xpath' => 'infsonewsroom:ContextProjectName',
       ],
       [
         'name' => 'project_website_url',
         'field' => 'field_newsroom_project_website/uri',
-        'xpath' => 'infsonewsroom:ContextProjectURL'
+        'xpath' => 'infsonewsroom:ContextProjectURL',
       ],
       [
         'name' => 'project_website_title',
         'field' => 'field_newsroom_project_website/title',
-        'xpath' => 'infsonewsroom:ContextProjectURLDisplay'
+        'xpath' => 'infsonewsroom:ContextProjectURLDisplay',
       ],
       [
         'name' => 'project_coordinator',
         'field' => 'field_newsroom_pr_coordinator',
-        'xpath' => 'infsonewsroom:ContextProjectCoordinator'
+        'xpath' => 'infsonewsroom:ContextProjectCoordinator',
       ],
       [
         'name' => 'venue',
         'field' => 'field_newsroom_venue',
-        'xpath' => 'infsonewsroom:ContextVenue'
+        'xpath' => 'infsonewsroom:ContextVenue',
       ],
       [
         'name' => 'organiser',
         'field' => 'field_newsroom_organiser',
-        'xpath' => 'infsonewsroom:ContextOrganiser'
+        'xpath' => 'infsonewsroom:ContextOrganiser',
       ],
       [
         'name' => 'author',
         'field' => 'field_newsroom_author',
-        'xpath' => 'infsonewsroom:ContextAuthor'
+        'xpath' => 'infsonewsroom:ContextAuthor',
       ],
       [
         'name' => 'speaker',
         'field' => 'field_newsroom_speaker',
-        'xpath' => 'infsonewsroom:ContextSpeaker'
+        'xpath' => 'infsonewsroom:ContextSpeaker',
       ],
       [
         'name' => 'registration_link_url',
         'field' => 'field_newsroom_registration_link/title',
-        'xpath' => 'infsonewsroom:ContextRegistrationLink'
+        'xpath' => 'infsonewsroom:ContextRegistrationLink',
       ],
       [
         'name' => 'registration_link_text',
         'field' => 'field_newsroom_registration_link/uri',
-        'xpath' => 'infsonewsroom:ContextRegistrationLinkText'
+        'xpath' => 'infsonewsroom:ContextRegistrationLinkText',
       ],
       [
         'name' => 'contact_text',
         'field' => 'field_newsroom_contact_text',
-        'xpath' => 'infsonewsroom:ContextContactText'
+        'xpath' => 'infsonewsroom:ContextContactText',
       ],
       [
         'name' => 'contact_info',
         'field' => 'field_newsroom_contact_info',
-        'xpath' => 'infsonewsroom:ContextContactEmail'
+        'xpath' => 'infsonewsroom:ContextContactEmail',
       ],
       [
         'name' => 'linked_object',
         'field' => 'field_newsroom_linked_object',
-        'xpath' => 'infsonewsroom:FullLinkedObject'
+        'xpath' => 'infsonewsroom:FullLinkedObject',
       ],
       [
         'name' => 'quote_box',
         'field' => 'field_newsroom_quote_box',
-        'xpath' => 'infsonewsroom:FullQuoteBox'
+        'xpath' => 'infsonewsroom:FullQuoteBox',
       ],
     ];
 
