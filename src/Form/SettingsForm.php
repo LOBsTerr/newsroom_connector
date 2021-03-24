0337cf97d9018591629703163e55faa6558863f9
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2021-03-24 15:43:07 +0100

    Add cron settings for importers.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -5,6 +5,7 @@ namespace Drupal\newsroom_connector\Form;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\newsroom_connector\Plugin\NewsroomProcessorManager;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -20,15 +21,34 @@ class SettingsForm extends ConfigFormBase {
   protected $httpClient;
 
   /**
-   * {@inheritdoc}
+   * Processor plugin manager service.
+   *
+   * @var \Drupal\newsroom_connector\Plugin\NewsroomProcessorManager
+   */
+  protected $newsroomProcessorPluginManager;
+
+  /**
+   * Universe manager.
+   *
+   * @var \Drupal\newsroom_connector\UniverseManager
+   */
+  protected $universeManager;
+
+  /**
+   * Settings form
    *
    * @param \GuzzleHttp\ClientInterface $http_client
    *   The Guzzle HTTP client.
+   * @param \Drupal\newsroom_connector\UniverseManager $universe_manager
+   *   Universe manager.
+   * @param \Drupal\newsroom_connector\Plugin\NewsroomProcessorManager $processor_plugin_manager
+   *   Processor plugin manager service.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, ClientInterface $http_client, UniverseManager $universeManager) {
+  public function __construct(ConfigFactoryInterface $config_factory, ClientInterface $http_client, UniverseManager $universe_manager, NewsroomProcessorManager $processor_plugin_manager) {
     parent::__construct($config_factory);
     $this->httpClient = $http_client;
-    $this->universeManager = $universeManager;
+    $this->universeManager = $universe_manager;
+    $this->newsroomProcessorPluginManager = $processor_plugin_manager;
   }
 
   /**
@@ -38,7 +58,8 @@ class SettingsForm extends ConfigFormBase {
     return new static(
       $container->get('config.factory'),
       $container->get('http_client'),
-      $container->get('newsroom_connector.universe_manager')
+      $container->get('newsroom_connector.universe_manager'),
+      $container->get('newsroom_connector.plugin.manager.newsroom_processor')
     );
   }
 
@@ -133,7 +154,35 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('docsroom_url'),
     ];
 
+    $plugins = $this->newsroomProcessorPluginManager->getDefinitions();
+
+    if (!empty($plugins)) {
+      $form['importers_settings'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Importer settings'),
+        '#open' => FALSE,
+      ];
+      foreach ($plugins as $plugin_id => $plugin) {
+        $form['importers_settings']["importer_setting_{$plugin_id}"] = [
+          '#type' => 'fieldset',
+          '#title' => $this->t('%plugin settings', [
+            '%plugin' => $plugin['label']->render(),
+          ]),
+        ];
+        $cron_import_setting_name = $this->getCronImortSettingName($plugin_id);
+        $form['importers_settings']["importer_setting_{$plugin_id}"][$cron_import_setting_name] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Import on cron'),
+          '#default_value' => $config->get($cron_import_setting_name) ?? 0,
+        ];
+      }
+    }
+
     return $form;
+  }
+
+  protected function getCronImortSettingName($plugin_id) {
+    return "cron_import_{$plugin_id}";
   }
 
   /**
@@ -141,7 +190,7 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $values = $form_state->getValues();
-    $this->config('newsroom_connector.settings')
+    $settings = $this->config('newsroom_connector.settings')
       ->set('universe_id', $values['universe_id'])
       ->set('base_url', $values['base_url'])
       ->set('allowed_ips', $values['allowed_ips'])
@@ -151,8 +200,15 @@ class SettingsForm extends ConfigFormBase {
       ->set('item_edit_segment', $values['item_edit_segment'])
       ->set('item_edit_script', $values['item_edit_script'])
       ->set('proposal_script', $values['proposal_script'])
-      ->set('docsroom_url', $values['docsroom_url'])
-      ->save();
+      ->set('docsroom_url', $values['docsroom_url']);
+
+    $plugins = $this->newsroomProcessorPluginManager->getDefinitions();
+    foreach ($plugins as $plugin_id => $plugin) {
+      $cron_import_setting_name = $this->getCronImortSettingName($plugin_id);
+      $settings->set($cron_import_setting_name, $values[$cron_import_setting_name]);
+    }
+
+   $settings->save();
 
     parent::submitForm($form, $form_state);
   }
